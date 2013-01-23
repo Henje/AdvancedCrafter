34ef10069987804b734a637f4fdde4387a2160a2
Henje <janhenrikm@yahoo.de>
2013-01-23 20:58:28 +0100

    build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,13 @@
+# git clone git://github.com/AdvancedCrafter/AdvancedCrafter.git
+mkdir build
+cp src build/src
+cp texture build/texture
+0cd build
 git clone git://github.com/BuildCraft/BuildCraft.git
 wget http://mcp.ocean-labs.de/files/mcp726a.zip
 unzip mcp726a.zip
 wget http://files.minecraftforge.net/minecraftforge/minecraftforge-src-latest.zip
-unzip `ls | find minecraftforge*`
+unzip minecraftforge-src-latest.zip
 chmod +x .
 cd forge
 ./install.sh
@@ -12,4 +17,6 @@ cd ..
 cp texture reobf/minecraft/texture
 cd reobf/minecraft
 zip AdvancedCrafter.zip texture henje
-mv AdvancedCrafter.zip ../../AdvancedCrafter_$VERSION.zip
+mv AdvancedCrafter.zip ../../../AdvancedCrafter_$VERSION.zip
+cd ../../../
+rm -r build
